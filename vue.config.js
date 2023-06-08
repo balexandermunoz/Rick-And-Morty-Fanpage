e702dbcdc53d7eaf096af4131097c2aa5dd74db7
Brayan Muñoz <balexander.munoz@udea.edu.co>
2023-06-08 13:11:54 -0500

    :bug: Domain Changed

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/guane-test-frontend/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/Rick-And-Morty-Fanpage/" : "/",
 };
